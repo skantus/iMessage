29a3feeeaf1e3415f955c8e3cf9b10cc2749dd73
skantus <drskantus@hotmail.com>
2020-04-19 21:55:32 +0200

    Setup project

---
+++ iMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMessage
+//
+//  Created by Alejo Castaño on 19/04/2020.
+//  Copyright © 2020 Alejo Castaño. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
